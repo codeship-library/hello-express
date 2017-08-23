fe38a4064eb50317130878be205df73d14261d57
Kelly J Andrews <kelly@kellyjandrews.com>
2017-08-23 09:55:32 -0400

    changed text

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello Something New!')
+  res.send('Hello Express!')
 });
 
 app.listen(port, function () {
